546f029caf3e7376a8ac518b5ddbebba59f2a99f
runneradmin <runneradmin@fv-az1400-618>
2025-01-24 20:54:19 +0000

    Add project files.

---
+++ VClockApp/Pages/Clock.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VClockApp.Pages
+{
+    public class ClockModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
